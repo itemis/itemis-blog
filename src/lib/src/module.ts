4ac5fef3041e90e4cfdf75c3988683b08d8652f3
Franz Becker <franz.becker@itemis.de>
2017-10-26 15:46:15 +0200

    add http dependency

--- src/lib/src/module.ts
+++ src/lib/src/module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { LibComponent } from './component/lib.component';
 import { LibService } from './service/lib.service';
@@ -6,6 +7,7 @@ import { LibService } from './service/lib.service';
 @NgModule({
   declarations: [LibComponent],
   providers: [LibService],
+  imports: [HttpModule],
   exports: [LibComponent]
 })
 export class LibModule { }
